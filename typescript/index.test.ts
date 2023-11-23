3ec8da37517e0ea050de7845fede7c2dff87e424
Guilherme Nandi Tiscoski <github@guilhermetiscoski.com>
2023-11-22 22:26:25 -0300

    add day 6 resolution and covering tests

--- typescript/index.test.ts
+++ typescript/index.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, test } from "bun:test";
 import { Game, Shape } from "./model/rps_game";
 import { checkSectionOverlap } from "./day_4";
+import { find_start_position } from "./day_6";
 
 test("day_2 - task_1 - rock_paper_scissor", () => {
   const game = new Game();
@@ -52,4 +53,22 @@ test("day_3 - task_2 - some overlapping cleaning sections", () => {
   expect(checkSectionOverlap(pairs_split[3].split(","), false)).toBe(true);
   expect(checkSectionOverlap(pairs_split[4].split(","), false)).toBe(true);
   expect(checkSectionOverlap(pairs_split[5].split(","), false)).toBe(true);
+});
+
+test("day_6 - task_1 - find position where last 4 characters are unique", () => {
+  expect(find_start_position('mjqjpqmgbljsphdztnvjfqwrcgsmlb',4)).toBe(7);
+  expect(find_start_position('bvwbjplbgvbhsrlpgdmjqwftvncz', 4)).toBe(5);
+  expect(find_start_position('nppdvjthqldpwncqszvftbrmjlhg', 4)).toBe(6);
+  expect(find_start_position('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 4)).toBe(10);
+  expect(find_start_position('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 4)).toBe(11);
+  expect(find_start_position('aaaaaaaaaaaaaaaaaaaaaaaaa', 4)).toBe(-1);
+});
+
+test("day_6 - task_1 - find position where last 14 characters are unique", () => {
+  expect(find_start_position('mjqjpqmgbljsphdztnvjfqwrcgsmlb',14)).toBe(19);
+  expect(find_start_position('bvwbjplbgvbhsrlpgdmjqwftvncz', 14)).toBe(23);
+  expect(find_start_position('nppdvjthqldpwncqszvftbrmjlhg', 14)).toBe(23);
+  expect(find_start_position('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 14)).toBe(29);
+  expect(find_start_position('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 14)).toBe(26);
+  expect(find_start_position('aaaaaaaaeaaaafaaabaaaaaaa', 14)).toBe(-1);
 });
