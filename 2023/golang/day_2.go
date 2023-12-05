e946394dfd0085e1f581db8f2bccfd3e2d0d9a1e
Guilherme Tiscoski <guilherme.tiscoski@asksuite.com>
2023-12-05 13:28:17 -0300

    done: day 2 task 2

--- 2023/golang/day_2.go
+++ 2023/golang/day_2.go
@@ -9,7 +9,8 @@ import (
 var games map[int]map[string]int
 
 func exec_day_2() {
-	fmt.Println(d2_task1())
+	// fmt.Println(d2_task1())
+	fmt.Println(d2_task2())
 }
 
 func d2_task1() int {
@@ -46,6 +47,33 @@ func d2_task1() int {
 	return sum
 }
 
-func d2_task2() {
+func d2_task2() int {
+	games, err := read_file(2, 1, true)
+	sum := 0
+	if err == nil {
+		for _, game := range games {
+			if len(game) > 0 {
+				game_split := strings.Split(game, ":")
+				game_map := map[string]int{
+					"blue":  0,
+					"red":   0,
+					"green": 0,
+				}
+				if err == nil {
+					cards_split := strings.Split(strings.ReplaceAll(game_split[1], ";", ","), ",")
+					for _, card := range cards_split {
+						card_split := strings.Split(strings.Trim(card, " "), " ")
+						quantity, _ := strconv.ParseInt(card_split[0], 10, 8)
+						color := card_split[1]
+						if quantity > int64(game_map[color]) {
+							game_map[color] = int(quantity)
+						}
+					}
+					sum += game_map["blue"] * game_map["red"] * game_map["green"]
+				}
+			}
+		}
+	}
+	return sum
 
 }
