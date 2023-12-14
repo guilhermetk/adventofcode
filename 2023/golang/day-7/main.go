b52a4a4b0f1c3d78075a26b7b47c6dae2b980168
Guilherme Tiscoski <guilherme.tiscoski@asksuite.com>
2023-12-14 08:47:09 -0300

    update 2023 folder structure; done day 7 task 1

---
+++ 2023/golang/day-7/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	task1()
+}
+
+var cardOrder map[string]int = map[string]int{
+	"A": 13,
+	"K": 12,
+	"Q": 11,
+	"J": 10,
+	"T": 9,
+	"9": 8,
+	"8": 7,
+	"7": 6,
+	"6": 5,
+	"5": 4,
+	"4": 3,
+	"3": 2,
+	"2": 1,
+}
+
+var hands []Hand
+
+//five of a kind:	AAAAA
+//four of a kind:	AAAAT
+//full house:		AAATT
+//three of a kind:	AAATY
+//two pair:			AATTX
+//one pair:			AATXY
+//high card:		23456
+
+type Hand struct {
+	cards [5]string
+	bid   int
+}
+
+func task1() {
+	// fileContent, _ := os.ReadFile("./test.txt")
+	fileContent, _ := os.ReadFile("./prod.txt")
+	lines := strings.Split(string(fileContent), "\n")
+	for _, line := range lines {
+		if len(line) > 0 {
+			cardSplit := strings.Split(line, " ")
+			cards := strings.Split(cardSplit[0], "")
+			bid, _ := strconv.Atoi(cardSplit[1])
+			hand := Hand{cards: [5]string(cards), bid: bid}
+			hands = append(hands, hand)
+		}
+	}
+	fmt.Println(hands)
+	slices.SortFunc(hands, func(a, b Hand) int {
+		if a.kind() < b.kind() {
+			return -1
+		}
+
+		if a.kind() > b.kind() {
+			return 1
+		}
+
+		return sortHandByCards(a, b)
+	})
+	sum := 0
+	for i := 0; i < len(hands); i++ {
+		sum += (i + 1) * hands[i].bid
+		// fmt.Println(i+1, hands[i].cards, hands[i].bid)
+	}
+	fmt.Println(sum)
+}
+
+func sortHandByCards(a, b Hand) int {
+	order := 0
+	for i := 0; i < 5; i++ {
+		aCard := cardOrder[a.cards[i]]
+		bCard := cardOrder[b.cards[i]]
+		if aCard > bCard {
+			return 1
+		}
+		if aCard < bCard {
+			return -1
+		}
+	}
+	return order
+}
+
+func (h Hand) kind() int {
+	groups := map[string]int{}
+	for _, card := range h.cards {
+		if groups[card] == 0 {
+			groups[card] = 1
+		} else {
+			groups[card] += 1
+		}
+	}
+	switch len(groups) {
+	case 5:
+		return 1 //high card
+	case 1:
+		return 7 // five of a kind
+	case 2:
+		if groups[h.cards[0]] == 4 || groups[h.cards[0]] == 1 {
+			return 6 // four of a kind
+		} else {
+			return 5 //full house
+		}
+	case 3:
+		for _, value := range groups {
+			if value == 2 {
+				return 3 //two pair
+			} else if value == 3 {
+				return 4 //three pair
+			}
+		}
+	case 4:
+		return 2 //one pair
+	}
+	return 0
+}
