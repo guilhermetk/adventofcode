c2bc8dcb552f0b825e35e113bdb43abfe109ea40
Guilherme Tiscoski <guilherme.tiscoski@asksuite.com>
2023-12-20 17:28:51 -0300

    wip: day 10 task 1

--- 2023/golang/day-10/main.go
+++ 2023/golang/day-10/main.go
@@ -11,33 +11,40 @@ type Position struct {
 	y int
 }
 
+var visitedPositions map[Position]bool
+
+// COMING FROM:
 // 0 - UP
 // 1 - RIGHT
 // 2 - DOWN
 // 4 - LEFT
-
-var pipeDirection = map[string][]Position{
-	"|": {{x: 0, y: 1}, {x: 0, y: 0}, {x: 0, y: -1}, {x: 0, y: 0}},
-	"-": {{x: 0, y: 0}, {x: -1, y: 0}, {x: 0, y: 0}, {x: 1, y: 0}},
-	"L": {{x: 1, y: 1}, {x: -1, y: -1}, {x: 0, y: 0}, {x: 0, y: 0}},
-	"J": {{x: -1, y: 1}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 1, y: -1}},
-	"7": {{x: 0, y: 0}, {x: 0, y: 0}, {x: -1, y: -1}, {x: 1, y: 1}},
-	"F": {{x: 0, y: 0}, {x: -1, y: 1}, {x: 1, y: -1}, {x: 0, y: 0}},
-	".": {{x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}},
+var pipeDirection = map[string][]bool{
+	"|": {true, false, true, false},
+	"-": {false, true, false, true},
+	"L": {false, false, true, true},
+	"J": {false, true, true, false},
+	"7": {true, true, false, false},
+	"F": {true, false, false, true},
+	".": {false, false, false, false},
+	"S": {false, false, false, false},
 }
 
 var moveDirections = [][]int{
-	{0, -1}, //UP
-	{1, 0},  //RIGHT
-	{0, 1},  //DOWN
-	{-1, 0}, //LEFT
+	{-1, 0}, //UP
+	{0, 1},  //RIGHT
+	{1, 0},  //DOWN
+	{0, -1}, //LEFT
 }
+
+var path []string = []string{}
 
 func main() {
 	file, _ := os.ReadFile("./test.txt")
 	lines := strings.Split(string(file), "\n")
 	var maze [][]string
 	var startPosition Position
+	visitedPositions = make(map[Position]bool)
+	fmt.Println(visitedPositions[Position{x: 1, y: 1}])
 	for index, line := range lines {
 		if len(line) > 0 {
 			maze = append(maze, strings.Split(line, ""))
@@ -52,18 +59,48 @@ func main() {
 }
 
 func task1(maze [][]string, startPosition Position) {
-	move(maze, startPosition, startPosition, 0)
+	fmt.Println(move(maze, startPosition, startPosition, 0))
 }
 
 func move(maze [][]string, startPosition Position, currPos Position, steps int) int {
-	// if steps >= 0 && maze[startPosition.x][startPosition.y] == maze[currPos.x][currPos.y] {
-	// 	return steps
-	// }
-	for i := 0; i < 4; i++ {
-		newPipe := maze[currPos.x+moveDirections[i][1]][currPos.y+moveDirections[i][0]]
-
+	visitedPositions[currPos] = true
+	currentPosPipe := maze[currPos.x][currPos.y]
+	if steps > 0 && currentPosPipe == "S" {
+		fmt.Println("Encerrou: ", steps)
+		return steps
 	}
-	return 0
+
+	maxSteps := steps
+
+	for i := 0; i < 4; i++ {
+		moveX := moveDirections[i][0]
+		moveY := moveDirections[i][1]
+		newPipePosition := Position{x: currPos.x + moveX, y: currPos.y + moveY}
+		newPipe := maze[currPos.x+moveX][currPos.y+moveY]
+		canMoveToPipe := pipeDirection[newPipe][i]
+		if !outOfBounds(maze, newPipePosition) && canMoveToPipe && !visitedPositions[newPipePosition] {
+			fmt.Println(newPipe)
+			newStep := move(maze, startPosition, newPipePosition, maxSteps+1)
+			if newStep > maxSteps {
+				maxSteps = newStep
+			}
+		}
+	}
+	return maxSteps
+}
+
+func outOfBounds(maze [][]string, pos Position) bool {
+	if pos.x < 0 || pos.y < 0 || pos.x > len(maze[0])-1 || pos.y > len(maze)-1 {
+		return true
+	}
+	return false
+}
+
+func canMoveToPipe(pos Position) bool {
+	if pos.x == 0 && pos.y == 0 {
+		return false
+	}
+	return true
 }
 
 func task2() {
