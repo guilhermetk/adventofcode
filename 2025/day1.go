f5a6ca1bd23ed3336043208d2a19853bf354c910
Guilherme Tiscoski <github@guilhermetiscoski.com>
2025-12-19 15:34:18 -0600

    D1 fix

---
+++ 2025/day1.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+func Day1(content []string) {
+	dialPos := 50
+	password := 0
+	zeroStepCounter := 0
+	for _, line := range content {
+		if line != "" {
+			steps := parseLine(line)
+			newDialPos, zeroStep := moveDial(dialPos, steps)
+			fmt.Println("dial moved to: ", newDialPos)
+			zeroStepCounter += zeroStep
+			dialPos = newDialPos
+			if newDialPos == 0 {
+				password++
+			}
+		}
+	}
+	fmt.Println("first password is: ", password)
+	fmt.Println("second password", zeroStepCounter)
+}
+
+func moveDial(dialPos, steps int) (int, int) {
+	movedSteps := 0
+	zeroStep := 0
+	for movedSteps < abs(steps) {
+		movedSteps++
+		if steps > 0 {
+			if dialPos == 99 {
+				dialPos = 0
+			} else {
+				dialPos++
+			}
+		} else {
+			if dialPos == 0 {
+				dialPos = 99
+			} else {
+				dialPos--
+			}
+		}
+		if dialPos == 0 {
+			zeroStep++
+		}
+	}
+
+	return dialPos, zeroStep
+}
+
+func abs(i int) int {
+	if i < 0 {
+		return i * -1
+	}
+
+	return i
+}
+
+func parseLine(l string) int {
+	direction := l[:1]
+	steps := l[1:]
+
+	convStep, err := strconv.Atoi(steps)
+	if err != nil {
+		log.Fatalf("error converting step to integer %s", steps)
+		return 0
+	}
+
+	if direction == "L" {
+		convStep *= -1
+	}
+
+	return convStep
+}
