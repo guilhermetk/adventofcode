43527855e5b2309af54fd0ee9a9c9ba6bfef71dd
Guilherme Tiscoski <github@guilhermetiscoski.com>
2025-12-23 11:14:02 -0600

    D2

--- 2025/day2.go
+++ 2025/day2.go
@@ -2,40 +2,52 @@ package main
 
 import (
 	"fmt"
-	"iter"
+	"slices"
 	"strconv"
 	"strings"
 )
 
 func Day2(content []string) {
 	input := content[0]
-	ranges := strings.SplitSeq(input, ",")
-	pt1Result := part1(ranges)
-	fmt.Println("part1: ", pt1Result)
+	ranges := slices.Collect(strings.SplitSeq(input, ","))
+	// pt1Result := part1(ranges)
+	// fmt.Println("part1: ", pt1Result)
 	pt2Result := part2(ranges)
 	fmt.Println("part2: ", pt2Result)
 }
 
-func part2(it iter.Seq[string]) int {
+func part2(it []string) int {
 	sum := 0
 
-	for r := range it {
+	for _, r := range it {
 		split := strings.Split(r, "-")
 		min, _ := strconv.Atoi(split[0])
 		max, _ := strconv.Atoi(split[1])
 
 		for i := min; i <= max; i++ {
 			text := strconv.Itoa(i)
+
+			for j := 1; j <= len(text)/2; j++ {
+				prefix := text[:j]
+				rest := text[j:]
+
+				result := strings.ReplaceAll(rest, prefix, "")
+				if result == "" {
+					sum += i
+					fmt.Println(text, "_", sum)
+					break
+				}
+			}
 		}
 	}
 
 	return sum
 }
 
-func part1(it iter.Seq[string]) int {
+func part1(it []string) int {
 	sum := 0
 
-	for r := range it {
+	for _, r := range it {
 		split := strings.Split(r, "-")
 		min, _ := strconv.Atoi(split[0])
 		max, _ := strconv.Atoi(split[1])
